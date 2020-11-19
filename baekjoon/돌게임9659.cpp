a61096dac718eee18ea43397e8d04cbf8665c6ca
LEE026 <58213749+LEE026@users.noreply.github.com>
2020-11-19 19:13:28 +0900

    원래 다이나믹으로 푸는 것 같지만 그렇게 하지 않아도 풀림

---
+++ baekjoon/돌게임9659.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+	long long n;
+
+	cin >> n;
+
+	if (n % 2L == 0)
+		printf("CY");
+	else
+		printf("SK");
+
+	
+}
