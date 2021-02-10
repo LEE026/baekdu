c14a4bc1ed21043120c477b7ef30bcfe692ea677
LEE026 <58213749+LEE026@users.noreply.github.com>
2021-02-10 17:52:53 +0900

    언어이해도 확인문제

---
+++ baekjoon/기찍N_2742.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i--)
+	{
+		cout << i << "\n";
+	}
+}
