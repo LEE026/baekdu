2ae37c28a504a12db4ad112a8c72152a6fe080a6
LEE026 <58213749+LEE026@users.noreply.github.com>
2021-08-05 19:34:18 +0900

    dp?

---
+++ baekjoon/퇴사_14501_dp.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<pii> arr(n);
+	vector<int> result(n+1);
+
+	for (auto& i : arr) {
+		cin >> i.first >> i.second;
+	}
+
+	for (int i = 0; i < n; i++) {
+		result[i] = max(result[max(0,i - 1)], result[i]);
+
+		int next = i + arr[i].first;
+
+		if (next <= n) {
+			result[next] = max(result[next], result[i] + arr[i].second);
+		}
+	}
+
+	cout << max(result[n],result[n-1]);
+
+}
